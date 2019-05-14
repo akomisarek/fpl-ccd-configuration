3875cf85aec6907a4238dd01286cca749b26bac7
Sivakumar Sirigiri <sivakumarsirigiri@Sivakumars-MacBook-Pro.local>
2019-05-14 11:18:18 +0100

    changed environment saucelabs variables

--- e2e/helpers/SauceLabsReportingHelper.js
+++ e2e/helpers/SauceLabsReportingHelper.js
@@ -8,8 +8,10 @@ const CONF = require('../../e2e/config.js');
 //const saucelabs_accesskey = 'privatekey';
 //const saucelabs_username = 'SivaK';
 //const saucelabs_accesskey = '65e1e5c6-ae4b-4432-9854-276fff0610d8';
-const sauceUsername = process.env.SAUCE_USERNAME || CONF.saucelabs.username;
-const sauceKey = process.env.SAUCE_ACCESS_KEY || CONF.saucelabs.key;
+//const sauceUsername = process.env.SAUCE_USERNAME || CONF.saucelabs.username;
+//const sauceKey = process.env.SAUCE_ACCESS_KEY || CONF.saucelabs.key;
+const sauceUsername = process.env.SAUCELABS_USERNAME || CONF.saucelabs.username;
+const sauceKey = process.env.SAUCELABS_ACCESS_KEY || CONF.saucelabs.key;
 
 function updateSauceLabsResult(result, sessionId) {
   console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=fpl-ccd-configuration'); /* eslint-disable-line no-console, prefer-template */
